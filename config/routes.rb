2e9c1247f1ba7a9dc54973a26ca8abd3c7ba2f85
joeromero47 <63633214+joeromero47@users.noreply.github.com>
2021-10-22 12:05:41 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, :posts, :comments, :sessions 
+  resources :users, only: [:create, :new, :show, :index]
+  resources :sessions, only: [:create]
+  resources :posts, :comments
 end
