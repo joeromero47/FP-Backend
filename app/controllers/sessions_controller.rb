2df79988ce36b22f0855ecbf009b7ef542f47e13
joeromero47 <63633214+joeromero47@users.noreply.github.com>
2021-10-22 12:02:22 -0700

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,17 @@
 class SessionsController < ApplicationController
     def create
-        
+        user = User
+            user = User.find_by(username: params['user']['username']).try(:authenticate, params['user']['password_digest'])
+        if user
+            session[:user_id] = user.id
+            render json: {
+                status: :created,
+                logged_in: true,
+                user: user
+            } 
+        else
+            render json: { status: 401 }
+        end
     end
 
 
